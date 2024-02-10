826dbe6b80cf5f2fe8f454fddd84cb3edc97b3bc
@AndrzejJava <andrzej.tyniecka@gmail.com>
2024-02-10 22:09:49 +0100

    IdDto

---
+++ src/main/java/com/example/englishapp/models/dto/IdDto.java
@@ -0,0 +1,13 @@
+package com.example.englishapp.models.dto;
+
+import lombok.*;
+
+@ToString
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class IdDto {
+    Integer id;
+}
